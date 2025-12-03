902afd828e178a4fa499139ebecfb48b4b4fd9ed
Sumukh <109731532+Sumukh2003@users.noreply.github.com>
2025-12-03 13:05:07 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (59.24%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        dict={}
+        for i,num in enumerate(nums):
+            diff=target-num
+            if diff in dict:
+                return [dict[diff],i]
+            dict[num]=i
+        return []
