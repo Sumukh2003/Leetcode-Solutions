f2064feebae6019bc62e265ec0b186ce3d53e551
Sumukh Marathe <109731532+Sumukh2003@users.noreply.github.com>
2025-12-11 21:13:29 +0530

    Time: 443 ms (85.05%) | Memory: 0B (100.00%) - LeetSync

--- 1179-game-play-analysis-i/game-play-analysis-i.sql
+++ 1179-game-play-analysis-i/game-play-analysis-i.sql
@@ -1,5 +1,5 @@
--- # Write your MySQL query statement below
--- select distinct player_id, min(event_date) as first_login from Activity group by player_id;
-SELECT player_id, MIN(event_date) first_login
-FROM Activity
-GROUP BY 1;
+# Write your MySQL query statement below
+select distinct player_id, min(event_date) as first_login from Activity group by player_id;
+
+
+
